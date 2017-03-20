d3ca05a27e3321517b48c784449adbf054994282
Aditya Inapurapu <adityaii@gmail.com>
2017-03-19 18:46:30 -0700

    Functional with junit. Needs refactoring.

--- src/main/java/com/iaditya/bdd/cucumber_sample/AnnotatedStepDefs.java
+++ src/main/java/com/iaditya/bdd/cucumber_sample/AnnotatedStepDefs.java
@@ -7,24 +7,24 @@ import cucumber.api.java.en.When;
 
 public class AnnotatedStepDefs {
 	
-    @Given("there are (\\d+) coffees left in the machine")
+    @Given("^there are (\\d+) coffees left in the machine$")
     public void thereAreNCoffeesLeftInTheMachine(int coffeeCups)
 	{
 		System.out.format("There are %n coffees left in the machine\n", coffeeCups);
 	}
     
-    @And("I have deposited (\\d+)$")
+    @And("^I have deposited (\\d+) dollars$")
     public void iHaveDepositedNDollars(Double dollars) {
 		System.out.format("I have deposited %n dollars\n", dollars);
     	
     }
     
-    @When("I press the coffee button")
+    @When("^I press the coffee button$")
     public void iPressTheCoffeeButton() {
     	System.out.println("I press the coffee button");
     }
     
-    @Then("I should be served a coffee")
+    @Then("^I should be served a coffee$")
     public void iShouldBeServedACoffee() {
     	System.out.println("I should be served coffee");
     }
--- src/main/java/com/iaditya/bdd/cucumber_sample/coffee.feature
+++ src/main/java/com/iaditya/bdd/cucumber_sample/coffee.feature
@@ -6,7 +6,7 @@ Feature: Serve coffee
 
 Scenario: Buy last coffee
   Given there are 1 coffees left in the machine
-  And I have deposited 1$
+  And I have deposited 1 dollars
   When I press the coffee button
   Then I should be served a coffee
   
