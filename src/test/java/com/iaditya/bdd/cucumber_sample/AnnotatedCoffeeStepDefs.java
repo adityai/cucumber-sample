bfd9ffadac109bfa887c1384d40f0b054c6a274f
Aditya Inapurapu <adityaii@gmail.com>
2017-03-20 07:12:48 -0700

    Adding parameterized test methods

--- src/test/java/com/iaditya/bdd/cucumber_sample/AnnotatedCoffeeStepDefs.java
+++ src/test/java/com/iaditya/bdd/cucumber_sample/AnnotatedCoffeeStepDefs.java
@@ -21,12 +21,38 @@ public class AnnotatedCoffeeStepDefs {
     
     @When("^I press the coffee button$")
     public void iPressTheCoffeeButton() {
-    	System.out.println("I press the coffee button");
+    	System.out.println("I press the coffee button\n");
     }
     
     @Then("^I should be served a coffee$")
     public void iShouldBeServedACoffee() {
-    	System.out.println("I should be served coffee");
+    	System.out.println("I should be served coffee\n");
+    }
+
+//	Commented out because it is implemented in the LambdaCoffeeStepDefs class as an example
+//    @Then("^I got (\\d+) cups of coffee$")
+//    public void iGotNCupsOfCoffee(int cups) {
+//		System.out.format("I got %n cups of coffee\n", cups);
+//    }
+
+    @Given("^The coffee machine serves latte, mocha and coffee$")
+    public void theCoffeeMachineServesLatteMochaAndCoffee() {
+    	System.out.println("The coffee machine serves latte, mocha and coffee\n");
+    }
+
+    @And("^I have deposited \"(\\d+)\" dollars$")
+    public void iHaveDepositedNDollars(float dollars) {
+		System.out.format("I have deposited %n dollars\n", dollars);
+    }    
+    
+    @When("^I press the \"([^\"]*)\" button$")
+    public void iPressTheSpecialtyCoffeeButton(String specialtyCoffee) {
+    	System.out.format("I press the %s button\n", specialtyCoffee);
+    }
+    
+    @Then("^I got \"(\\d+)\" cups of \"([^\"]*)\"$")
+    public void iGotNParameterizedCupsOfCoffee(int cups, String specialtyCoffee) {
+    	System.out.format("I got %n cups of %s\n", cups, specialtyCoffee);
     }
 
 }
--- src/test/java/com/iaditya/bdd/cucumber_sample/coffee.feature
+++ src/test/java/com/iaditya/bdd/cucumber_sample/coffee.feature
@@ -4,15 +4,26 @@ Feature: Serve coffee
   Coffee should not be served until the button has been pressed
   If there is no coffee left then money should be refunded
 
-Scenario: Buy last coffee
+Scenario: Order last coffee
   Given there are 1 coffees left in the machine
   And I have deposited 1 dollars
   When I press the coffee button
   Then I should be served a coffee
   
-Scenario: Buy last two coffees
+Scenario: Order last two coffees
   Given there are 2 coffees left in the machine
   And I have deposited 2 dollars
   When I press the coffee button
   Then I got 2 cups of coffee
   
+Scenario Outline: Order specialty coffees
+  Given The coffee machine serves latte, mocha and coffee
+  And I have deposited "<dollars>" dollars
+  When I press the "<specialtyCoffee>" button
+  Then I got "<cups>" cups of "<specialtyCoffee>"
+  
+  Examples:
+  |  dollars 	|	specialtyCoffee	|	cups	|
+  |  2			|	latte			|	1		|
+  |  4			|	mocha			|	1		|
+  |  6			|	coffee			|	6		|
