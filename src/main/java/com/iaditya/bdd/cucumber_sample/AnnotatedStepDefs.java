c2ff343c5eae0211080726174aea96e90bcaa985
Aditya Inapurapu <adityaii@gmail.com>
2017-03-19 18:37:31 -0700

    Preparing annotated coffee step definition

--- src/main/java/com/iaditya/bdd/cucumber_sample/AnnotatedStepDefs.java
+++ src/main/java/com/iaditya/bdd/cucumber_sample/AnnotatedStepDefs.java
@@ -1,13 +1,32 @@
 package com.iaditya.bdd.cucumber_sample;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class AnnotatedStepDefs {
 	
-    @Given("I have (\\d+) cups of coffees in my belly")
-    public void I_have_coffee_in_my_belly(int coffeeCups)
-   		{
-    			System.out.format("Coffee cups: %n\n", coffeeCups);
-   		}
+    @Given("there are (\\d+) coffees left in the machine")
+    public void thereAreNCoffeesLeftInTheMachine(int coffeeCups)
+	{
+		System.out.format("There are %n coffees left in the machine\n", coffeeCups);
+	}
+    
+    @And("I have deposited (\\d+)$")
+    public void iHaveDepositedNDollars(Double dollars) {
+		System.out.format("I have deposited %n dollars\n", dollars);
+    	
+    }
+    
+    @When("I press the coffee button")
+    public void iPressTheCoffeeButton() {
+    	System.out.println("I press the coffee button");
+    }
+    
+    @Then("I should be served a coffee")
+    public void iShouldBeServedACoffee() {
+    	System.out.println("I should be served coffee");
+    }
 
 }
